0a97581150e4fd36c9923a1ceada7bec0fc7e1cb
Michael <michael.turner.iot@gmail.com>
2019-01-02 12:09:13 +1100

    rework after threading attempt

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,28 @@
 #include <Arduino.h>
 
 #include <Adafruit_NeoPixel.h>
+
 #include <IRremoteESP8266.h>
 #include <IRrecv.h>
 #include <IRutils.h>
+
 
 #ifdef __AVR__
   #include <avr/power.h>
 #endif
 
-#define PIN 5
-
-
-// An IR detector/demodulator is connected to GPIO pin 4(D2 on a NodeMCU
-// board).
-const uint16_t kRecvPin =14;
+#define ledDataPin 5 // GPIO pin 5 (D1 on a NodeMCU board).
+// An IR detector/demodulator is connected to GPIO pin 4 (D2 on a NodeMCU board).
+const uint16_t kRecvPin = 4;
 
 IRrecv irrecv(kRecvPin);
 
 decode_results results;
 
-// Parameter 1 = number of pixels in strip
-// Parameter 2 = Arduino pin number (most are valid)
-// Parameter 3 = pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(63, PIN, NEO_GRB + NEO_KHZ800);
-
-
-
 const int  CYCLE_MILLISECONDS = 5000; // 5 second breathing cycle.
+
+const int striplength = 62; // number of leds on strip
+
 const uint8_t KEYFRAMES[]  = {
   // Rising
   20, 21, 22, 24, 26, 28, 31, 34, 38, 41, 45, 50, 55, 60, 66, 73, 80, 87, 95,
@@ -52,97 +42,116 @@ const uint32_t BASECOLORS[] = {
   242, 247, 251, 254, 255
 };
 
-uint8 baseColorIndex = 0;
-uint32_t basecolor = BASECOLORS[baseColorIndex];
+// variables used by breathe
 
 unsigned long lastBreath = 0.0;
 int keyframePointer = 0;
-uint32_t breatheLength = 63/3;
+uint32_t breatheLength = striplength/3;
 uint32_t cyclecount = 0;
 
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(striplength, ledDataPin, NEO_GRB + NEO_KHZ800);
 
-// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
-// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
-// and minimize distance between Arduino and first pixel.  Avoid connecting
-// on a live circuit...if you must, connect GND first.
-
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  }
-  if(WheelPos < 170) {
-    WheelPos -= 85;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-  WheelPos -= 170;
-  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-}
-
-// Start methods
-
-void breathe(uint32_t midColor) {
-  int numKeyframes = sizeof(KEYFRAMES) - 1;
-  float period = CYCLE_MILLISECONDS / numKeyframes;
-  unsigned long now = millis();
-
-  if ((now - lastBreath) > period) {
-
-    lastBreath = now;
-    for (uint32_t i = breatheLength; i < strip.numPixels() - breatheLength; i++){
-      strip.setPixelColor(i, midColor);
+  uint32_t Wheel(byte WheelPos) {
+    WheelPos = 255 - WheelPos;
+    if(WheelPos < 85) {
+      return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
     }
-
-    for (uint32_t i = 0; i < breatheLength; i++) {
-      uint8_t color = (127 * KEYFRAMES[keyframePointer]) / 256;
-      strip.setPixelColor(i, color, 0, color);
-      strip.setPixelColor(strip.numPixels() - breatheLength + i, color, 0, color);
+    if(WheelPos < 170) {
+      WheelPos -= 85;
+      return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
     }
+    WheelPos -= 170;
+    return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  }
+  // Start methods
+
+  void breathe(uint32_t midColor) {
+    int numKeyframes = sizeof(KEYFRAMES) - 1;
+    float period = CYCLE_MILLISECONDS / numKeyframes;
+    unsigned long now = millis();
+
+    if ((now - lastBreath) > period) {
+
+      lastBreath = now;
+      for (uint32_t i = breatheLength; i < strip.numPixels() - breatheLength; i++){
+        strip.setPixelColor(i, midColor);
+      }
+
+      for (uint32_t i = 0; i < breatheLength; i++) {
+        uint8_t color = (127 * KEYFRAMES[keyframePointer]) / 256;
+        strip.setPixelColor(i, color, 0, color);
+        strip.setPixelColor(strip.numPixels() - breatheLength + i, color, 0, color);
+      }
 
 
-    strip.show();
+      strip.show();
 
-    // Increment the keyframe pointer.
-    if (++keyframePointer > numKeyframes) {
-      // Reset to 0 after the last keyframe.
-      keyframePointer = 0;
+      // Increment the keyframe pointer.
+      if (++keyframePointer > numKeyframes) {
+        // Reset to 0 after the last keyframe.
+        keyframePointer = 0;
+      }
     }
   }
-}
 
-// Fill the dots one after the other with a color
-void colorWipe(uint32_t c, uint8_t wait) {
+  void rainbow(uint8_t wait) {
+    uint16_t i, j;
+
+    for(j=0; j<256; j++) {
+      for(i=0; i<strip.numPixels(); i++) {
+        strip.setPixelColor(i, Wheel((i+j) & 255));
+      }
+      strip.show();
+      delay(wait);
+    }
+  }
+
+  // Slightly different, this makes the rainbow equally distributed throughout
+  void rainbowCycle(uint8_t wait) {
+    uint16_t i, j;
+
+    for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
+      for(i=0; i< strip.numPixels(); i++) {
+        strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
+      }
+      strip.show();
+      delay(wait);
+    }
+  }
+
+  //Theatre-style crawling lights with rainbow effect
+  void theaterChaseRainbow(uint8_t wait) {
+    for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
+      for (int q=0; q < 3; q++) {
+        for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+          strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
+        }
+        strip.show();
+
+
+        delay(wait);
+
+        for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
+          strip.setPixelColor(i+q, 0);        //turn every third pixel off
+        }
+      }
+    }
+  }
+
+void colorWipe(uint32_t c, uint8_t _wait) {
   for(uint16_t i=0; i<strip.numPixels(); i++) {
     strip.setPixelColor(i, c);
     strip.show();
-    delay(wait);
-  }
-}
-
-void rainbow(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256; j++) {
-    for(i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel((i+j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }
-}
-
-// Slightly different, this makes the rainbow equally distributed throughout
-void rainbowCycle(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
-    for(i=0; i< strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
-    }
-    strip.show();
-    delay(wait);
+    // wait before next pixel
+    delay(_wait);
   }
 }
 
@@ -164,25 +173,6 @@ void theaterChase(uint32_t c, uint8_t wait) {
   }
 }
 
-//Theatre-style crawling lights with rainbow effect
-void theaterChaseRainbow(uint8_t wait) {
-  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
-    for (int q=0; q < 3; q++) {
-      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
-      }
-      strip.show();
-
-
-      delay(wait);
-
-      for (uint16_t i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, 0);        //turn every third pixel off
-      }
-    }
-  }
-}
-
 void setup() {
   // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
   #if defined (__AVR_ATtiny85__)
@@ -190,54 +180,56 @@ void setup() {
   #endif
   // End of trinket special code
 
+  Serial.begin(115200);
   irrecv.enableIRIn();  // Start the receiver
 
-  strip.Color(127, 0, 0);
-
+  strip.Color(0, 0, 0);
   strip.begin();
   strip.show(); // Initialize all pixels to 'off'
+
 }
 
 void loop() {
 
   if (irrecv.decode(&results)) {
+    String hexResults = uint64ToString(results.value, HEX);
     // print() & println() can't handle printing long longs. (uint64_t)
-//    serialPrintUint64(results.value, HEX);
-    if ( strip.getBrightness() > 10){
-      strip.setBrightness(strip.getBrightness() - 10);
-    } else {
-      strip.setBrightness(0);
+    if (hexResults.length() == 6) {
+      //Serial.println(hexResults);
+      if ( strip.getBrightness() > 10){
+        strip.setBrightness(strip.getBrightness() - 10);
+      } else {
+        strip.setBrightness(0);
+      }
     }
-//    Serial.println("");
     irrecv.resume();  // Receive the next value
   }
+
   // Some example procedures showing how to display to the pixels:
-  //colorWipe(strip.Color(255, 0, 0), 50); // Red
-  //colorWipe(strip.Color(0, 255, 0), 50); // Green
-  //colorWipe(strip.Color(0, 0, 255), 50); // Blue
-
-  cyclecount++;
-  if (cyclecount < 6){
-    breathe( strip.Color(127, 0, 0) );
-  } else if (cyclecount < 11){
-    breathe( strip.Color(127, 0, 0) );
-  } else if (cyclecount < 16){
-    breathe( strip.Color(127, 0, 0) );
-  } else {
-    cyclecount = 0;
-  }
 
 
+  colorWipe(strip.Color(32, 0, 0), 50); // Red
+  colorWipe(strip.Color(32, 32, 0), 50);
+  colorWipe(strip.Color(0, 32, 0), 50); // Green
+  colorWipe(strip.Color(0, 32,32), 50);
+  colorWipe(strip.Color(0, 0, 32), 50); // Blue
+    // custom color??
+    //  colorWipe(strip.Color(0, 0, 127), 50); // Blue
 
-
-
-
-
-
-
-
+ // cyclecount++;
+ // if (cyclecount < 6){
+ //   breathe( strip.Color(32, 32, 32) );
+ // } else if (cyclecount < 11){
+ //   breathe( strip.Color(32, 32, 32) );
+ // } else if (cyclecount < 16){
+ //   breathe( strip.Color(32, 32, 32) );
+ // } else {
+ //   cyclecount = 0;
+ // }
 
 //colorWipe(strip.Color(0, 0, 0, 255), 50); // White RGBW
+
+
   // Send a theater pixel chase in...
 //  theaterChase(strip.Color(127, 127, 127), 50); // White
 //  theaterChase(strip.Color(127, 0, 0), 50); // Red
@@ -246,4 +238,5 @@ void loop() {
 //  rainbow(20);
 //  rainbowCycle(20);
 //  theaterChaseRainbow(50);
+  delay(10);
 }
